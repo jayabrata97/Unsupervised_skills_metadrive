0326f57ecfefff6559a0e52f525756853bb928c3
Jayabrata <joyj03787@gmail.com>
2022-04-22 10:37:23 +0530

    updated evaluation code

--- metadrive_dads_sd_no_encoder/dads_evaluation.py
+++ metadrive_dads_sd_no_encoder/dads_evaluation.py
@@ -9,6 +9,7 @@ from metadrive_networks_v3 import *
 from metadrive_ppo_agent_v3 import PPOAgent
 from metadrive import MetaDriveEnv
 #from metadrive import SafeMetaDriveEnv
+from metadrive.constants import TerminationState
 import torch as T
 from torch.nn.functional import mse_loss
 from torch.utils.data import DataLoader
@@ -30,13 +31,12 @@ def sample_skills(skill_dim):
 
     return rv
 
-def evaluate(skill_dynamics, actor, critic, num_episodes, env):
+def evaluate(skill_dynamics, actor, num_episodes, env):
     ret_reward = []
     ret_length = []
-    ret_success_rate = []
-    ret_out_rate = []
-    ret_crash_vehicle_rate = []
-    start = time.time()
+    #ret_success_rate = []
+    #ret_out_rate = []
+    #ret_crash_vehicle_rate = []
     episode_count = 0
     planning_horizon = 1 #H_P
     primitive_holding = 25 #H_Z
@@ -52,6 +52,9 @@ def evaluate(skill_dynamics, actor, critic, num_episodes, env):
                                 [-1.0, 0.0],
                                 [-1.0, -1.0]], dtype=T.float, device=device_1)
 
+    success_rate = 0
+    out_rate = 0
+    crash_vehicle_rate = 0
     while episode_count < num_episodes:
         obs = env.reset()
         done = False
@@ -70,8 +73,11 @@ def evaluate(skill_dynamics, actor, critic, num_episodes, env):
         # print(diff_trajs)
         # print(type(diff_trajs[0]))
         ########################################################
-
+        episode_reward = 0
+        episode_length = 0
         while done != True:
+            velocity = []
+            #cost = 0  #cost for SafeMetaDrive
             obs = T.tensor(obs, dtype=T.float, device=skill_dynamics.device)
             print(obs)
             costs = []
@@ -83,24 +89,47 @@ def evaluate(skill_dynamics, actor, critic, num_episodes, env):
             for j in range(0, primitive_holding):
                 action, _ = actor.sample_normal(obs, chosen_skill)
                 obs, reward, done, info = env.step(action)
+                episode_reward += reward
+                episode_length = episode_length+1
+                velocity.append(info["velocity"])
+                if info["arrive_dest"]:
+                    # ret_success_rate.append(1)
+                    success_rate += 1
+                if info["out_of_road"]:
+                    # ret_out_rate.append(1)
+                    out_rate += 1
+                if info["crash_vehicle"]:
+                    # ret_crash_vehicle_rate.append(1)
+                    crash_vehicle_rate += 1    
                 if done:
                     break
 
         episode_count += 1
+        ret_reward.append(episode_reward)
+        ret_length.append(episode_length)
 
-    return reward
+    ret = dict(
+        reward = np.mean(ret_reward),
+        length = np.mean(ret_length),
+        # success_rate = np.mean(ret_success_rate),
+        # out_rate = np.mean(ret_out_rate),
+        # crash_vehicle_rate = np.mean(ret_crash_vehicle_rate),
+        success_rate = success_rate / num_episodes,
+        out_rate = out_rate / num_episodes,
+        crash_vehicle_rate = crash_vehicle_rate / num_episodes,
+    )
+
+    return ret
 
 if __name__ == "__main__":
-    #skill_dynamics= SkillDynamics()
-    skill_dynamics = T.load('/home/airl-gpu4/Jayabrata/Unsupervised_skills_metadrive/metadrive_dads_sd_no_encoder/models/dads_metadrive/PPO_nonlog_intri_reward/PPOskill_dynamics_L500_nonlog.pt')
+    skill_dynamics= SkillDynamics()
+    skill_dynamics = T.load('/home/airl-gpu4/Jayabrata/Unsupervised_skills_metadrive/metadrive_dads_sd_no_encoder/models/dads_metadrive/PPO_LogIntriRew_L500/PPOskill_dynamics_eps0.2_epc30_L500.pt')
     #print(skill_dynamics)
     skill_dynamics = skill_dynamics.eval()
-    actor = T.load('/home/airl-gpu4/Jayabrata/Unsupervised_skills_metadrive/metadrive_dads_sd_no_encoder/models/dads_metadrive/PPO_nonlog_intri_reward/PPOactor_L500_nonlog.pt')
+    actor = ActorNetwork()
+    actor = T.load('/home/airl-gpu4/Jayabrata/Unsupervised_skills_metadrive/metadrive_dads_sd_no_encoder/models/dads_metadrive/PPO_LogIntriRew_L500/PPOactor_eps0.2_epc30_L500.pt')
     #print(actor)
     actor = actor.eval()
-    critic = T.load('/home/airl-gpu4/Jayabrata/Unsupervised_skills_metadrive/metadrive_dads_sd_no_encoder/models/dads_metadrive/PPO_nonlog_intri_reward/PPOcritic_L500_nonlog.pt')
-    #print(critic)
-    critic = critic.eval()
 
     env = MetaDriveEnv(dict(
         # controller="joystick",
@@ -110,12 +139,14 @@ if __name__ == "__main__":
         random_traffic = False, 
         environment_num=200,
         start_seed=0,
-        random_lane_width=True,
+        random_lane_width=False,#True,
+        random_lane_num=False, #True,
         random_agent_model=True,
-        random_lane_num=True,
-        map=7
+        map=3,#7
     ))
 
-    num_episodes = 500
+    num_episodes = 20
 
-    ret = evaluate(skill_dynamics, actor, critic, num_episodes, env)
+    ret = evaluate(skill_dynamics, actor, num_episodes, env)
+    print("Evaluation result: {}".format(ret))
+    
