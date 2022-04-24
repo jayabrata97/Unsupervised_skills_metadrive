a7c816f13947a553b859c1e7fb9f33d37963e8d4
Jayabrata <joyj03787@gmail.com>
2022-04-24 21:34:33 +0530

    update for checking

--- trial2.py
+++ trial2.py
@@ -2,7 +2,7 @@ from metadrive import MetaDriveEnv
 from metadrive import SafeMetaDriveEnv
 import argparse
 import random
-#import gym
+import gym
 import numpy as np
 
 traffic_density_sample = np.random.uniform(0.5, 0.9)
@@ -11,20 +11,20 @@ if traffic_density_sample < 0.5:
 
 env = MetaDriveEnv(dict(
     # controller="joystick",
-    use_render=True,
-    # manual_control=True,
-    traffic_density= np.random.uniform(0.5, 0.9), #0.1
+    use_render= True,
+    manual_control=True,
+    traffic_density= 0.1,
     random_traffic = False, 
-    environment_num=1000,
-    start_seed=1000,
+    environment_num=200,
+    start_seed=0,
     #start_seed=random.randint(0, 1000)
-    random_lane_width=True,
+    random_lane_width=False,
     random_agent_model=True,
-    random_lane_num=True,
+    random_lane_num=False,
     map=7
 ))
 
-# env = gym.make("MetaDrive-100envs-v0")
+# env = gym.make("MetaDrive-validation-v0")
 # env = MetaDriveEnv(dict(environment_num=1000))  # Or you can also choose to create env from class.
 
 print("\nThe action space: {}".format(env.action_space))
@@ -33,21 +33,25 @@ print("Starting the environment ...\n")
 
 ep_reward = 0.0
 obs = env.reset()
-for i in range(10000):
+for i in range(100000000):
     obs, reward, done, info = env.step(env.action_space.sample())
     #print("obs type:",type(obs))
-    print("step reward:",reward, end='\r')
+    #print("step reward:",reward)
+    #print("Current velocity: ", info["velocity"])
     ep_reward += reward
+    if done:
+        break
     #if done:
-    if (i%100) == 0:
-        print("\nThe episode reward: ", ep_reward)
-        print("\nThe returned information: {}.".format(info))
-        obs = env.reset()
+    # if (i%100) == 0:
+    #     print("\nThe episode reward: ", ep_reward)
+    #     print("\nThe returned information: {}.".format(info))
+    #     obs = env.reset()
         #break
 
 print("\nThe observation shape: {}.".format(obs.shape))
-print("\nThe returned reward: {}.".format(reward))
+print("\nThe returned episode reward: {}.".format(ep_reward))
 print("\nThe returned information: {}.".format(info))
+print('i:', i)
 
 env.close()
 print("\nMetaDrive successfully run!")
